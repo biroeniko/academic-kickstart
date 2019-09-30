365acb62685796d2283305968e4ac8177f7269b4
Biro Eniko <encybiro@gmail.com>
2019-09-30 17:27:40 +0300

    The brew-installed hugo is now running with executing the view.sh script.

--- view.sh
+++ view.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
 hugo --i18n-warnings server
