3bd993f406621cbb6e1003a3ceee017b338154c2
Biro Eniko <encybiro@gmail.com>
2019-10-01 22:34:30 +0300

    scripts are correctedgit add .git add .!

--- view.sh
+++ view.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
 hugo --i18n-warnings server
